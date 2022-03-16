ceb53d3211e63f6937dbcc2d7b291946551b79bb
Tucker Steil <tuckersteil@gmail.com>
2022-03-15 17:04:49 -0700

    commit tree 7c9a8a54c455811e166836772e6eff1624734946

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x){
+  if (x <= 400){
+    return "This one is on me!"
+  } else if (x < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if (x > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default: 
+      return "Bye."
+  }
 }
